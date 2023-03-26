7de1505597a68eba0265e7ebb827e83380c83d1d
azamiya <t.adaniya@adawarp.com>
2023-03-26 16:03:00 +0900

    initial commit

---
+++ rosbag/rosbag_record.sh
@@ -0,0 +1,2 @@
+ros2 bag record topics /kentora/status/BatteryLevel /kentora/status/EcuMode /sensing/gnss/fix /vehicle/status/control_mode /vehicle/status/gear_status /vehicle/status/steering_status /vehicle/status/velocity_status
+
